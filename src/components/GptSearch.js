97a55ed8f4f14fb80ee8ca6d13ebad94b1f76eed
reddyprashanthn <reddyprashanth7080@gmail.com>
2025-06-26 17:45:26 +0530

    updated Backgorund URL

--- src/components/GptSearch.js
+++ src/components/GptSearch.js
@@ -2,12 +2,13 @@ import React from 'react'
 import GptSearchBar from './GptSearchBar'
 import GptMovieSuggestions from './GptMovieSuggestions'
 import { BG_URL } from '../utils/constant'
+import BG from "./images/netflix.jpg"
 
 const GptSearch = () => {
   return (
     <div>
       <div className="fixed -z-10">
-        <img src= {BG_URL} 
+        <img src= {BG} 
         alt = "backgroundpage"/>
       </div>
         <GptSearchBar />
